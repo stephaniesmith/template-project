56ce7d792e5b24a8f26f95984804cbe676c585e8
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-07 11:22:56 -0700

    updated app props

--- js/components/app.js
+++ js/components/app.js
@@ -5,6 +5,7 @@ let template = function() {
     return html`
         <header>
             <h1>Demo Project</h1>
+            <div></div>
         </header>
 
         <main></main>
@@ -12,10 +13,35 @@ let template = function() {
 };
 
 export default class App {
+    constructor() {
+        this.count = 0;
+    }
     render() {
         let dom = template();
 
-        const counter = new Counter();
+        let appCount = dom.querySelector('header > div');
+        appCount.textContent = this.count;
+
+        let counterProps = {
+            count: this.count,
+            onDecrement: () => {
+                this.count --;
+                appCount.textContent = this.count;
+                const updateProps = {
+                    count: this.count
+                };
+                counter.update(updateProps);
+            },
+            onIncrement: () => {
+                this.count ++;
+                appCount.textContent = this.count;
+                const updateProps = {
+                    count: this.count
+                };
+                counter.update(updateProps);            }
+        };
+
+        const counter = new Counter(counterProps);
         const main = dom.querySelector('main');
         main.appendChild(counter.render());
 
